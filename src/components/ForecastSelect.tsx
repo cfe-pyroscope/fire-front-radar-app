d7b1c69a0dc00b7c170fa0815bff59aefc83eb13
Caporlingua Marina <marina.caporlingua@ilsole24ore.com>
2025-06-27 10:57:46 +0200

    minor changes

--- src/components/ForecastSelect.tsx
+++ src/components/ForecastSelect.tsx
@@ -26,7 +26,7 @@ const ForecastSelect: React.FC<ForecastSelectProps> = ({
     return (
         <div className="forecast-select-cnt">
             <Select
-                label="Forecast time"
+                label="Most recent forecast"
                 placeholder="Pick forecast time"
                 value={selectedLeadHours?.toString() ?? null}
                 onChange={(value) => {
