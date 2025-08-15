51bfa0a16cf7ce82132872c3be3539fab364572b
Caporlingua Marina <marina.caporlingua@ilsole24ore.com>
2025-08-15 15:14:33 +0200

    basic test of time series chart

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,16 +1,22 @@
+// src/routes/Router.tsx
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from '../layouts/Layout';
 import Home from '../pages/Home';
+import ChartTest from '../pages/ChartTest';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+        {/* Routes with the Sidenav */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
         </Route>
+
+        {/* Route without the Sidenav */}
+        <Route path="/chart-test" element={<ChartTest />} />
       </Routes>
     </BrowserRouter>
   );
